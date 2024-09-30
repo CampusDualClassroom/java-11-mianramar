46fb49cee67a60a93d5fe5d1069947958eef9d0e
Miguel Ángel Rama Martínez <mianramar@gmail.com>
2024-09-30 10:14:33 +0200

    Ejercicio resuelto

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -2,10 +2,25 @@ package com.campusdual.classroom;
 
 public class Exercise11 {
     public static void main(String[] args) {
-        TVRemote redRemote = new TVRemote("Rojo");
         TVRemote blackRemote = new TVRemote("Negro");
-        System.out.println(redRemote.getColor());
+
+        blackRemote.turnOn();
+
+        blackRemote.channelUp();
+
+        blackRemote.channelDown();
+
+        blackRemote.channelDown();
+
+        blackRemote.volumeUp();
+
+        blackRemote.volumeDown();
+
+        blackRemote.volumeDown();
+
         System.out.println(blackRemote.getColor());
+
+        blackRemote.turnOff();
     }
 
 }
